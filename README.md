ef2c8f83991b12cf50834412f6dbbeafb9c48525
Den Pol <selection4test@gmail.com>
2019-07-27 23:20:45 +0300

    BrowserWindow will be created when localhost:3000 will be available in dev mode

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 Electron side will be installed then frontend side will be installed.
 
 ### `yarn dev`
-You can use CRA dev mode in electron app. **But!** Need to reload the app when `http://localhost:3000` will be ready. _We're working on it..._
+You can use CRA dev mode in electron app. Electron app BrowserWindow will be created when `http://localhost:3000` will be available.
 
 ### `yarn fresh-prod-start`
 Fresh frontend will be built to `frontend/build` then be used.
@@ -37,5 +37,47 @@ if (!dev) {
   mainWindow.loadFile('./frontend/build/index.html');
 } else {
   mainWindow.loadURL('http://localhost:3000');
+}
+```
+- Add file `polling-to-frontend.js` for have ability to check is `http://localhost:3000` available.
+- Add code to `main.js` _(WAY 1 should be replaced to WAY 2)_.
+```javascript
+const axios = require('axios');
+
+const createPollingByConditions = require('./polling-to-frontend').createPollingByConditions;
+const CONFIG = {
+  FRONTEND_DEV_URL: 'http://localhost:3000',
+  FRONTEND_FIRST_CONNECT_INTERVAL: 4000,
+  FRONTERN_FIRST_CONNECT_METHOD: 'get'
+};
+let connectedToFrontend = false;
+
+// WAY 1: Without checking conditions.
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+// app.on('ready', createWindow);
+
+// WAY 2: Need to check something conditions.
+// I'm gonna to check is CONFIG.FRONTEND_DEV_URL resource available then create window...
+if (dev) {
+  createPollingByConditions ({
+    url: CONFIG.FRONTEND_DEV_URL,
+    method: CONFIG.FRONTERN_FIRST_CONNECT_METHOD,
+    toBeOrNotToBe: () => !connectedToFrontend,
+    interval: CONFIG.FRONTEND_FIRST_CONNECT_INTERVAL,
+    callbackAsResolve: () => {
+      console.log(`SUCCESS! CONNECTED TO ${CONFIG.FRONTEND_DEV_URL}`);
+      connectedToFrontend = true;
+
+      createWindow();
+    },
+    callbackAsReject: () => {
+      console.log(`FUCKUP! ${CONFIG.FRONTEND_DEV_URL} IS NOT AVAILABLE YET!`);
+      console.log(`TRYING TO RECONNECT in ${CONFIG.FRONTEND_FIRST_CONNECT_INTERVAL / 1000} seconds...`);
+    }
+  });
+} else {
+  app.on('ready', createWindow);
 }
 ```
--- main.js
+++ main.js
@@ -2,10 +2,19 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const dev = process.env.NODE_ENV === 'development';
+const axios = require('axios');
+
+const createPollingByConditions = require('./polling-to-frontend').createPollingByConditions;
+const CONFIG = {
+  FRONTEND_DEV_URL: 'http://localhost:3000',
+  FRONTEND_FIRST_CONNECT_INTERVAL: 4000,
+  FRONTERN_FIRST_CONNECT_METHOD: 'get'
+};
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+let connectedToFrontend = false;
 
 function createWindow () {
   // Create the browser window.
@@ -14,15 +23,23 @@ function createWindow () {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
-  })
+    },
+    // show: false
+  });
 
   if (!dev) {
     // and load the index.html of the app.
     mainWindow.loadFile('./frontend/build/index.html');
   } else {
+    // mainWindow.webContents.openDevTools();
     mainWindow.loadURL('http://localhost:3000');
   }
+
+  // if (true) {
+  //   mainWindow.once('ready-to-show', () => {
+  //     mainWindow.show();
+  //   });
+  // }
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -36,22 +53,46 @@ function createWindow () {
   })
 }
 
+// WAY 1: Without checking conditions.
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+// app.on('ready', createWindow);
+
+// WAY 2: Need to check something conditions.
+// I'm gonna to check is CONFIG.FRONTEND_DEV_URL resource available then create window...
+if (dev) {
+  createPollingByConditions ({
+    url: CONFIG.FRONTEND_DEV_URL,
+    method: CONFIG.FRONTERN_FIRST_CONNECT_METHOD,
+    toBeOrNotToBe: () => !connectedToFrontend,
+    interval: CONFIG.FRONTEND_FIRST_CONNECT_INTERVAL,
+    callbackAsResolve: () => {
+      console.log(`SUCCESS! CONNECTED TO ${CONFIG.FRONTEND_DEV_URL}`);
+      connectedToFrontend = true;
+
+      createWindow();
+    },
+    callbackAsReject: () => {
+      console.log(`FUCKUP! ${CONFIG.FRONTEND_DEV_URL} IS NOT AVAILABLE YET!`);
+      console.log(`TRYING TO RECONNECT in ${CONFIG.FRONTEND_FIRST_CONNECT_INTERVAL / 1000} seconds...`);
+    }
+  });
+} else {
+  app.on('ready', createWindow);
+}
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
-})
+});
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+  if (mainWindow === null) createWindow();
 })
 
 // In this file you can include the rest of your app's specific main process
