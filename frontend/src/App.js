e61e14984ca8dcb621ccebc6f322e76149c60468
Den Pol <selection4test@gmail.com>
2019-08-10 19:44:33 +0300

    Updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. window.externalFunction is {typeof window.externalFunction}
+          Edit <code>src/App.js</code> and save to reload.
+          <br />
+          <code>window.externalFunction</code> is {typeof window.externalFunction}
         </p>
         <a
           className="App-link"
